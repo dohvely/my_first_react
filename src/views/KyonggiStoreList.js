216a3f35eee88845087e2d850172b1a5840cc12a
unknown <chode8703@gmail.com>
2020-05-17 16:14:08 +0900

    #경지화 1.layout style

--- src/views/KyonggiStoreList.js
+++ src/views/KyonggiStoreList.js
@@ -1,16 +1,30 @@
 import React from 'react'
-// import '../../css/KyonggiStoreList.css'
+import './../css/KyonggiStoreList.css'
 
 function KyonggiStoreList() {
-  return <section>
-    <div className="header">
-      <img src="https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/120/twitter/247/money-with-wings_1f4b8.png" className="header_image" alt="money" />
-      <span>경기지역화폐 목록</span>
-    </div>
-    <div className="content">
-      <span>TODO</span>
-    </div>
-  </section>
+  return <section className="content">
+      <header>
+        <p>
+          <img src="https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/120/twitter/247/money-with-wings_1f4b8.png" alt="money" />
+          경기지역화폐
+        </p>
+      </header>
+
+      <div className="content_wrapper">
+        <div className="content_left">
+          <p>content_left</p>
+          <p>content_left</p>
+          <p>content_left</p>
+          <p>content_left</p>
+          <p>content_left</p>
+        </div>
+        <aside className="content_right">
+          <p>aside</p>
+        </aside>
+      </div>
+
+      <footer>footer</footer>
+    </section>
 }
 
 export default KyonggiStoreList
